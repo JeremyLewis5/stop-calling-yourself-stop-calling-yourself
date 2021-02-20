98abee2f1faf8fd1f086acff0b6079fee095942d
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-02-20 17:45:01 -0600

    changes

---
+++ main.js
@@ -0,0 +1,59 @@
+function threeqs(str) {
+    return str + '???';
+}
+
+let result1 = threeqs('hello');
+let result2 = threeqs('what even');
+result1;
+result2;
+
+
+function twoStrings(str1, str2) {
+    return str1 + ' ' + str2;
+    
+}
+
+let result3 = twoStrings('oh', 'hi');
+let result4 = twoStrings('well', 'hello back');
+result3;
+result4;
+
+function subtract3(num1, num2, num3) {
+    return num1 - num2 - num3;
+}
+
+let result5 = subtract3(10, 1, 2);
+let result6 = subtract3(8, 3, 5);
+let result7 = subtract3(100, 80, 25);
+result5;
+result6;
+result7
+
+function subtractreverse(num1, num2, num3) {
+    return num3 - num2 - num1; 
+}
+
+let result8 = subtractreverse(10 , 1, 2);
+let result9 = subtractreverse(2, 1, 10);
+result8;
+result9;
+
+function triple(str3) {
+    return str3 + str3 + str3;
+}
+
+let result10 = triple('codeimmersives');
+let result11 = triple('is the best');
+result10;
+result11;
+
+function sqrt(x) {
+    return x**(0.5) ;
+}
+
+let result12 = sqrt(16);
+let result13 = sqrt(4);
+let result14 = sqrt(9);
+result12;
+result13;
+result14;
